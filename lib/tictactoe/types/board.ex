f43be6e5f94e2cc931d77ab06d4a05f5fd4894c3
Ryan Leung <ryanleung@berkeley.edu>
2019-03-02 01:17:50 -0800

    latest

--- lib/tictactoe/types/board.ex
+++ lib/tictactoe/types/board.ex
@@ -2,21 +2,29 @@
 # TODO: let's explore how the API will look first before designing the board
 #
 # iex> board = %Board{}
-# iex> new_board_1 = Board.place_x(board, ROW_TOP, COL_LEFT)
-# iex> new_board_2 = Board.place_o(new_board_1, ROW_MID, COL_LEFT)
+# iex> board_1, result = Board.move(board, {1,1}, :x)
+# iex> board_2, result = Board.move(board_1, {2,2}, :y)
 # iex>
 defmodule Board do
+  @type row :: integer
+  @type col :: integer
+
   defstruct [
     free: MapSet.new([{1,1},{1,2},{1,3},{2,1},{2,2},{2,3},{3,1},{3,2},{3,3}]),
     x: MapSet.new(),
     o: MapSet.new(),
   ]
 
-  @spec move(Board, {integer, integer}, atom) :: Board
-  def move(board, field, player) do
+  @spec move(Board, row, col, atom) :: {Board, boolean()}
+  def move(board, row, col, player) do
     %{board |
-      :free => MapSet.delete(board.free, field),
-      player => MapSet.put(Map.get(board, player), field)
+      :free => MapSet.delete(board.free, {row, col}),
+      player => MapSet.put(Map.get(board, player), {row, col})
     }
+  end
+
+  @spec get_result(Board) :: {boolean(), atom}
+  def get_result(board) do
+    # UNDEFINED
   end
 end
